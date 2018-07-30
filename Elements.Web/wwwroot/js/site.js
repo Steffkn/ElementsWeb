00190ca37ff6078c43bc40cadc2fefb3730b8687
steffkn <steff.kn@abv.bg>
2018-07-30 18:24:20 +0300

    Added: News template to the project Fix: UI for news, Layout, Login, Register

--- Elements.Web/wwwroot/js/site.js
+++ Elements.Web/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function outsideView() {
+    $("footer").hide();
+    $("#navigation").addClass("fixed-top");
+    $("body").addClass("bg-outsider");
+}
